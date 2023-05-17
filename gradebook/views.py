dcc73ef1e5ae5700321c567456c1364da1a44a56
SinaKakhki <ksina.72@gmail.com>
2023-05-17 22:29:36 +1200

    initial views added will need configuration after adding templates

--- gradebook/views.py
+++ gradebook/views.py
@@ -1,3 +1,122 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Semester, Course, Class, Lecturer, Student, StudentEnrollment
 
-# Create your views here.
+# View for creating a semester (Step 1)
+def create_semester(request):
+    if request.method == 'POST':
+        # Process form submission and create a new semester
+        # ...
+        return redirect('semester_list')
+    return render(request, 'create_semester.html')
+
+# View for updating a semester (Step 1)
+def update_semester(request, semester_id):
+    semester = Semester.objects.get(pk=semester_id)
+    if request.method == 'POST':
+        # Process form submission and update the semester
+        # ...
+        return redirect('semester_list')
+    return render(request, 'update_semester.html', {'semester': semester})
+
+# View for deleting a semester (Step 1)
+def delete_semester(request, semester_id):
+    semester = Semester.objects.get(pk=semester_id)
+    # Process the deletion of the semester
+    # ...
+    return redirect('semester_list')
+
+# View for displaying a list of semesters (Step 1)
+def semester_list(request):
+    semesters = Semester.objects.all()
+    return render(request, 'semester_list.html', {'semesters': semesters})
+
+# View for creating a course (Step 2)
+def create_course(request):
+    if request.method == 'POST':
+        # Process form submission and create a new course
+        # ...
+        return redirect('course_list')
+    return render(request, 'create_course.html')
+
+# View for updating a course (Step 2)
+def update_course(request, course_id):
+    course = Course.objects.get(pk=course_id)
+    if request.method == 'POST':
+        # Process form submission and update the course
+        # ...
+        return redirect('course_list')
+    return render(request, 'update_course.html', {'course': course})
+
+# View for deleting a course (Step 2)
+def delete_course(request, course_id):
+    course = Course.objects.get(pk=course_id)
+    # Process the deletion of the course
+    # ...
+    return redirect('course_list')
+
+# View for displaying a list of courses (Step 2)
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'course_list.html', {'courses': courses})
+
+# View for creating a class (Step 3)
+def create_class(request):
+    if request.method == 'POST':
+        # Process form submission and create a new class
+        # ...
+        return redirect('class_list')
+    return render(request, 'create_class.html')
+
+# View for updating a class (Step 3)
+def update_class(request, class_id):
+    class_obj = Class.objects.get(pk=class_id)
+    if request.method == 'POST':
+        # Process form submission and update the class
+        # ...
+        return redirect('class_list')
+    return render(request, 'update_class.html', {'class_obj': class_obj})
+
+# View for deleting a class (Step 3)
+def delete_class(request, class_id):
+    class_obj = Class.objects.get(pk=class_id)
+    # Process the deletion of the class
+    # ...
+    return redirect('class_list')
+
+# View for displaying a list of classes (Step 3)
+def class_list(request):
+    classes = Class.objects.all()
+    return render(request, 'class_list.html', {'classes': classes})
+
+# View for creating a lecturer (Step 4)
+def create_lecturer(request):
+    if request.method == 'POST':
+        # Process form submission and create a new lecturer
+        # ...
+        return redirect('lecturer_list')
+    return render(request, 'create_lecturer.html')
+
+# View for updating a lecturer (Step 4)
+def update_lecturer(request, lecturer_id):
+    lecturer = Lecturer.objects.get(pk=lecturer_id)
+    if request.method == 'POST':
+        # Process form submission and update the lecturer
+        # ...
+        return redirect('lecturer_list')
+    return render(request, 'update_lecturer.html', {'lecturer': lecturer})
+
+# View for deleting a lecturer (Step 4)
+def delete_lecturer(request, lecturer_id):
+    lecturer = Lecturer.objects.get(pk=lecturer_id)
+    # Process the deletion of the lecturer
+    # ...
+    return redirect('lecturer_list')
+
+# View for displaying a list of lecturers (Step 4)
+def lecturer_list(request):
+    lecturers = Lecturer.objects.all()
+    return render(request, 'lecturer_list.html', {'lecturers': lecturers})
+
+# Other views for the remaining steps to be added here
+# ...
+
